1490edbc7769f001972b58194042a18d6cd2af5c
Vusoni <valoenu.contact@gmail.com>
2025-12-21 17:25:02 +0100

    Components and Hooks completed.

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+import UserDropdown from "@/components/UserDropdown";
+import {searchStocks} from "@/lib/actions/finnhub.actions";
+
+// Header component with async user prop 
+const Header = async ({ user }: { user: User }) => {
+    const initialStocks = await searchStocks(); // Fetch stocks
+
+    // UI classes comes from custom naming in globals.css
+    return (
+        <header className="sticky top-0 header">
+            <div className="container header-wrapper">
+                <Link href="/">
+                    <Image src="/assets/icons/logo.svg" alt="Application logo" width={140} height={32} className="cursor-pointer h-8 w-auto " />
+                </Link>
+                <nav className="hidden sm:block">
+                    <NavItems initialStocks={initialStocks} />
+                </nav>
+
+                <UserDropdown user={user} initialStocks={initialStocks} />
+            </div>
+        </header>
+    )
+}
+export default Header
